df06f5e046b708f20b7432792d9429cd42620717
Nolann Biron <ponyswapfather@gmail.com>
2022-03-13 22:17:56 +0100

    update

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import { return200, return500 } from "../utils/response";
-
-export default async function (req: VercelRequest, res: VercelResponse): Promise<void> {
-    res.send({title: "OK"})
-}
